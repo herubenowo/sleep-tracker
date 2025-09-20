d7900db9c2fa3c28f26b147e9d343e2d2d57ed61
Heru Benowo <heru.benowo@yahoo.co.id>
2025-09-20 22:42:50 +0700

    UPDATE: simplify repositories base

--- app/repositories/followings_repository/base.rb
+++ app/repositories/followings_repository/base.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 module FollowingsRepository
-  class Base
+  class Base < ::ApplicationRepository
     def initialize(params)
-      raise "params is invalid" if params.class != Hash && params.class != Hashie::Mash
+      super(params)
 
       @model = ::UserFollowing
-      @params = params
-    end
-
-    def handle_meta(page, per_page, total)
-      {
-        "pagination" => {
-          "page" => page,
-          "per_page" => per_page,
-          "total" => total
-        }
-      }
     end
   end
 end
--- app/repositories/sleep_record_repository/base.rb
+++ app/repositories/sleep_record_repository/base.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 module SleepRecordRepository
-  class Base
+  class Base < ::ApplicationRepository
     def initialize(params)
-      raise "params is invalid" if params.class != Hash && params.class != Hashie::Mash
+      super(params)
 
       @model = ::SleepRecord
-      @params = params
-    end
-
-    def handle_meta(page, per_page, total)
-      {
-        "pagination" => {
-          "page" => page,
-          "per_page" => per_page,
-          "total" => total
-        }
-      }
     end
   end
 end
